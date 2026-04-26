c5de0f20ecb96c65a7126b2436e6aeb42d585104
rezguimalak1-web <Rezguimalak1@gmail.com>
2026-04-27 00:58:51 +0100

    US5/6: upgrade event with status, capacity and rules

--- src/com/campusevents/models/main.java
+++ src/com/campusevents/models/main.java
@@ -1,5 +1,18 @@
 package com.campusevents.models;
 
+
 public class main {
+    public static void main(String[] args) {
+
+        Evenement ev = new Evenement("Hackathon", "2026", "ESPRIT");
+        ev.afficherDetails();
+        ev.modifierEvenement("Workshop Java", "Salle B");
+        ev.annulerEvenement();
+
+        // test important
+       ev.modifierEvenement("Test", "Salle C");
+
+        ev.afficherDetails();
+    }
     
 }
