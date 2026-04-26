24e74d8ee113e8deb0e60d0fd8adfa2e84bf8049
ayoub7876 <ayoub.krifi@etudiant-fsegt.utm.tn>
2026-04-26 20:08:56 +0100

    Add: event validation functionality (US13)

--- src/com/campusevents/models/Administrateur.java
+++ src/com/campusevents/models/Administrateur.java
@@ -2,5 +2,10 @@ package com.campusevents.models;
 
 public class Administrateur {
 
-
+public void validerEvenement(Evenement ev) {
+        System.out.println("Admin valide l'événement: " + ev.titre);
+    }
+public void validerEvenement(String event) {
+        System.out.println("Admin valide : " + event);
+    }
 }
