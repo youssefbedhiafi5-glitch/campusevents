42eb4ae0411df62c337a6caf3bd2efe2ba383e82
Eya-Ghrairi <ghrairi20@gmail.com>
2026-04-26 17:38:47 +0100

    Add: user class with attributes (name, email)

---
+++ src/com/campusevents/models/Utilisateur.java
@@ -0,0 +1,11 @@
+package com.campusevents.models;
+
+public class Utilisateur {
+    String nom;
+    String email;
+
+    public Utilisateur(String nom, String email) {
+        this.nom = nom;
+        this.email = email;
+    }
+}
