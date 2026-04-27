6bcf8aec5e40332eae99e7fc2937eb1cb0e897b2
adem-ab33 <adem.abassi@etudiant-fsegt.utm.tn>
2026-04-27 01:24:52 +0100

    US11: add participation logic with rules and capacity

--- src/com/campusevents/models/Participation.java
+++ src/com/campusevents/models/Participation.java
@@ -1,7 +1,25 @@
 package com.campusevents.models;
 
 public class Participation {
+
     public void participer(Utilisateur user, Evenement ev) {
-        System.out.println(user.nom + " participe à " + ev.titre);
+
+        // Vérification 1 : événement annulé
+        if (ev.statut.equals("ANNULE")) {
+            System.out.println("Impossible : événement annulé");
+            return;
+        }
+
+        // Vérification 2 : capacité atteinte
+        if (ev.participants >= ev.capacite) {
+            System.out.println(" Impossible : événement complet");
+            return;
+        }
+
+        // Logique métier : ajouter participant
+        ev.participants++;
+
+        System.out.println(""+ user.nom + " participe à " + ev.titre);
+        System.out.println("Participants : " + ev.participants + "/" + ev.capacite);
     }
 }
